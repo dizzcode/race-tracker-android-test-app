013162191ddfda5e298cf27f0889cb7fa3ba2f64
Shavinda Dissanayake <shavinda.diz@gmail.com>
2024-09-28 17:14:55 +0530

    Add all resources

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# race-tracker-android-test-app
+# Race Tracker android test app
+
+### What's inside >
+
+- Coroutines in Android Studio
+
+##
+### View
+##
+
+<img
+src="https://github.com/dizzcode/race-tracker-android-test-app/blob/main/screenshots/img.png"
+width="320"
+height="680"
+/>
--- app/src/main/java/dizzcode/com/racetracker/MainActivity.kt
+++ app/src/main/java/dizzcode/com/racetracker/MainActivity.kt
@@ -5,43 +5,24 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import dizzcode.com.racetracker.ui.RaceTrackerApp
 import dizzcode.com.racetracker.ui.theme.RaceTrackerTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
         setContent {
             RaceTrackerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                ) {
+                    RaceTrackerApp()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RaceTrackerTheme {
-        Greeting("Android")
     }
 }
